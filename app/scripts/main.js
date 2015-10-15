4ccd28be8e9ff1f0745739a75b6977075fee4225
Sean Dillingham <sean.dillingham@gmail.com>
2015-10-14 23:22:20 -0700

    more styling of slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   'use strict';
   $('.slider').slick({
+    dots: true,
+    fade: true,
+    speed: 800,
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icon-slider-next.svg" /></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icon-slider-prev.svg" /></button>'
   });
 });
