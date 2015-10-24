a0885ad5038a2976d3849e0d3eb298b8379dba8d
Sean Dillingham <sean.dillingham@gmail.com>
2015-10-24 16:07:48 -0700

    add Waypoints for triggering video plays; hide videos for smaller screens

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,10 @@
+/*jshint unused:false*/
+/*jshint -W117 */
+
 $(document).ready(function(){
   'use strict';
+
+  // Initialize slider
   $('.slider').slick({
     dots: true,
     fade: true,
@@ -8,8 +13,25 @@ $(document).ready(function(){
     prevArrow: '<button type="button" class="slick-prev"><img src="images/icon-slider-prev.svg" /></button>'
   });
 
+  // toggle mobile menu
   $('.icon-menu, .icon-close').click(function() {
     $('.global.fixed').toggleClass('active');
     $('body').toggleClass('locked');
+  });
+
+  // initialize waypoints
+  var triggerSimplicity = new Waypoint.Inview({
+    element: $('#simplicity'),
+    entered: function(direction) {
+      var videoSimplicity = document.getElementById('video-simplicity');
+      videoSimplicity.play();
+    }
+  });
+  var triggerOrganized = new Waypoint.Inview({
+    element: $('#organized'),
+    entered: function(direction) {
+      var videoOrganized = document.getElementById('video-organized');
+      videoOrganized.play();
+    }
   });
 });
