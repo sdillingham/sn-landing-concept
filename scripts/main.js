83f9cf49434873d44b8555d3b26116cee7340db6
Sean Dillingham <sean.dillingham@gmail.com>
2015-11-05 21:25:40 -0800

    Built simplenote from commit cdfce48 on branch master

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+/*jshint unused:false*/
+/*jshint -W117 */
+
+$(document).ready(function(){
+  'use strict';
+
+  // Initialize slider
+  $('.slider').slick({
+    dots: true,
+    fade: true,
+    speed: 800,
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icon-slider-next.svg" /></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icon-slider-prev.svg" /></button>'
+  });
+
+  // toggle mobile menu
+  $('.icon-menu, .icon-close, .global.fixed .nav-item a').click(function() {
+    $('.global.fixed').toggleClass('active');
+    $('body').toggleClass('locked');
+  });
+
+  // initialize waypoints
+  var triggerSimplicity = new Waypoint.Inview({
+    element: $('#simplicity'),
+    entered: function(direction) {
+      var videoSimplicity = document.getElementById('video-simplicity');
+      videoSimplicity.play();
+      this.destroy();
+    }
+  });
+  var triggerOrganized = new Waypoint.Inview({
+    element: $('#organized'),
+    entered: function(direction) {
+      var videoOrganized = document.getElementById('video-organized');
+      videoOrganized.play();
+      this.destroy();
+    }
+  });
+});
